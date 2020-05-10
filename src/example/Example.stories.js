943feb117093beea5490289f8e9beb155330e664
Mads Boddum <madsboddum@outlook.dk>
2020-05-10 12:54:04 +0200

    Hovering over a TextField no longer replaces the Wolfenstein cursor with the OS text cursor

--- src/example/Example.stories.js
+++ src/example/Example.stories.js
@@ -4,6 +4,7 @@ import Container from '../components/container/Container';
 import Cursor from '../components/cursor/Cursor';
 import Select from '../components/select/Select';
 import Text from '../components/text/Text';
+import TextField from '../components/textfield/TextField';
 
 export default {
   title: 'Example'
@@ -19,6 +20,7 @@ export const Basic = () => (
                 <Select label={'Selectable 1'} options={['Option 1', 'Option 2', 'Option 3']}/>
                 <Select label={'Selectable 2'} options={['Option 1', 'Option 2', 'Option 3']}/>
                 <Select label={'Selectable 3'} options={['Option 1', 'Option 2', 'Option 3']}/>
+                <div style={{display: 'flex'}}><Text>Input:</Text> <TextField/></div>
             </Container>
             <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
                 <Button>Yes</Button>
